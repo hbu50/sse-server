b14566dd1ed80a80f403fecf1fac61a3927ed9f1
Hossein Bukhamseen <bukhamseen.h@gmail.com>
2014-03-07 17:51:18 +0300

    handle exceptions in stream

--- lib/sse/server/version.rb
+++ lib/sse/server/version.rb
@@ -1,5 +1,5 @@
 module Sse
   module Server
-    VERSION = "0.0.3"
+    VERSION = "0.0.4"
   end
 end
--- lib/sse/server/web.rb
+++ lib/sse/server/web.rb
@@ -26,29 +26,35 @@ module Sse
         headers 'Access-Control-Allow-Origin'=> '*'
 
         stream(:keep_open) do |connection|
-          if last_event_id
-            #check redis
-            redis= Redis.connect
-            members=redis.zrangebyscore(channel,last_event_id,last_event_id)
-            if members.count > 0
-              members=redis.zrangebyscore(channel,last_event_id,'+inf')
-              Sse::Server.configuration.logger.info("Send Old events(count: #{members.count}).")
-              members.each do |m|
-                connection << Sse::Server.message_to_sse(m)
+          begin
+            if last_event_id
+              #check redis
+              redis= Redis.connect
+              members=redis.zrangebyscore(channel,last_event_id,last_event_id)
+              if members.count > 0
+                members=redis.zrangebyscore(channel,last_event_id,'+inf')
+                Sse::Server.configuration.logger.info("Send Old events(count: #{members.count}).")
+                members.each do |m|
+                  connection << Sse::Server.message_to_sse(m)
+                end
+              else# client is outdated
+                Sse::Server.configuration.logger.info("Client Outdated from #{last_event_id}.")
+                connection << Sse::Server.pack_as_sse(nil, 'control', {type: "error", error: "outdated"})
+                connection.close
               end
-            else# client is outdated
-              Sse::Server.configuration.logger.info("Client Outdated from #{last_event_id}.")
-              connection << Sse::Server.pack_as_sse(nil, 'control', {type: "error", error: "outdated"})
-              connection.close
             end
+            EventMachine::PeriodicTimer.new(25) { connection << ":\n" } # required, otherwise the connection is closed in 30-60 sec
+            
+            Sse::Server.configuration.connection_manager.subscribe(connection,channel)
+            connection.callback {
+              Sse::Server.configuration.logger.error("event-stream client disconnected. Channel: #{nnel}")
+              Sse::Server.configuration.connection_manager.unsubscribe(connection,channel)
+            }
+          rescue ::Exception => e
+            Sse::Server.configuration.logger.fatal("Exception in stream: ")
+            Sse::Server.configuration.logger.fatal(e.backtrace)
+            connection.close
           end
-          EventMachine::PeriodicTimer.new(25) { connection << ":\n" } # required, otherwise the connection is closed in 30-60 sec
-          
-          Sse::Server.configuration.connection_manager.subscribe(connection,channel)
-          connection.callback {
-            Sse::Server.configuration.logger.error("event-stream client disconnected. Channel: #{nnel}")
-            Sse::Server.configuration.connection_manager.unsubscribe(connection,channel)
-          }
         end
       end
 
